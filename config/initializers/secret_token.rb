06c84112b779c361636592f1d33ae6e70b46f610
RadwaElsersy <radwa.elsersy@usytech.com>
2016-08-07 15:20:42 +0200

    Create hafazny application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hafazny::Application.config.secret_token = 'be3d10f8a1c60c297010ed90ae162090908aec6f2573f7a5c8bb66b8c649aaacbc14baad3d00b421c56e45c11d3913bdfa298f36505604b8ab1b8553724bab17'
